f3b4bbb7faf97aa46a721d308661c3903f379644
Joshua Tauberer <jt@occams.info>
2016-01-29 19:06:53 -0500

    the templates have to be included in the setup package, bump version

--- setup.py
+++ setup.py
@@ -8,10 +8,12 @@ install_requires = [
 ]
 
 setup(name='django-html-emailer',
-      version='0.0.1',
+      version='0.0.2',
       description='Utility for sending HTML emails from Django.',
       author='Joshua Tauberer',
       author_email='jt@occams.info',
       install_requires=install_requires,
       packages=['htmlemailer'],
-      provides=['htmlemailer'])
+      provides=['htmlemailer'],
+      package_data={'htmlemailer': ['templates/htmlemailer/*']},
+)
